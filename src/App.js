a6206bc14cf6225ef61c01adb40f24eee1dea693
PlayerNexus45 <80288457+PlayerNexus45@users.noreply.github.com>
2023-11-23 21:22:29 +0100

    OHIO TAKES FORM

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
 import './App.css';
-import 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Login from './components/login.components';
+
 function App() {
   return (
-    <div className='login'>
-      <input type='text'className='form-control mt-1'placeholder='enter username'/>
+    <div className='App'>
+      <div className='auth-wrapper'>
+        <div className='auth-inner'>
+      <form>
+        <h3>Welcome to OhioIDU</h3>
+        <div className="mb-3">
+          <label>Login</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder=""
+          />
+        </div>
+        <div className="mb-3">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder=""
+          />
+        </div>
+        <div className="mb-3">
+          <div className="custom-control custom-checkbox">
+            <input
+              type="checkbox"
+              className="custom-control-input"
+              id="customCheck1"
+            />
+            <label className="custom-control-label" htmlFor="customCheck1">
+              Remember me
+            </label>
+          </div>
+        </div>
+        <div className="d-grid">
+          <button type="submit" className="btn btn-primary">
+            Glory to Ohio
+          </button>
+        </div>
+        <p className="forgot-password text-right">
+          Forgot <a href="https://www.youtube.com/watch?v=Zt-Ql9ZE2XI">password?</a>
+        </p>
+      </form>
+      </div>
+      </div>
     </div>
   );
 }
