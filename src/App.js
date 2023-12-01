76afcfa8e903b0771c0f651d8555b9fdee2d270c
PlayerNexus45 <micmac100606@gmail.com>
2023-12-01 14:08:48 +0100

    idu takes form

--- src/App.js
+++ src/App.js
@@ -9,70 +9,110 @@ const baseURL = "https://api.chucknorris.io/jokes/random"
 
 function App() {
   const [data, setData] = useState({data: []});
+  const x = 12;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [token, setToken] = useState('')
+  
 
-  const handleClick = async () => {
-      const response = await fetch(baseURL, {
-        method: 'GET',
-        headers: {
-          Accept: 'application/json',
-        },
-      });
 
-      const result = await response.json();
-
-      setData(result);
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
   };
 
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    axios.post(`https://s35.idu.edu.pl/api/v2/auth/login`, {login: `${email}`, password: `${password}`})
+      .then((response) =>{
+        setToken(response.data.token)
+      }
+      )
+  };
+  // const handleClick = async (e) => {
+  //   // e.preventDefault()
+  //   //    const response = await fetch(baseURL, {
+  //   //      method: 'GET',
+  //   //      headers: {
+  //   //        Accept: 'application/json',
+  //   //      },
+  //   //    });
+
+  //   //   const result = response
+
+  //   //   setData(result);
+
+
+  // };
+
   console.log(data);
-  
-  return (
-    <div className='App'>
-      <div className='auth-wrapper'>
-        <div className='auth-inner'>
-      <form>
-        <h3>Welcome to OhioIDU</h3>
-        <div className="mb-3">
-          <label>Login</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder=""
-          />
-        </div>
-        <div className="mb-3">
-          <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder=""
-          />
-        </div>
-        <div className="mb-3">
-          <div className="custom-control custom-checkbox">
+  if(token == ''){
+    return (
+      <div className='App'>
+        <div className='auth-wrapper'>
+          <div className='auth-inner'>
+        <form onSubmit={handleSubmit}>
+          <h3>Welcome to OhioIDU</h3>
+          <div className="mb-3">
+            <label>Login</label>
             <input
-              type="checkbox"
-              className="custom-control-input"
-              id="customCheck1"
+              type="text"
+              className="form-control"
+              value={email}
+              placeholder=""
+              onChange={handleEmailChange}
             />
-            <label className="custom-control-label" htmlFor="customCheck1">
-              Remember me
-            </label>
           </div>
+          <div className="mb-3">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control"
+              value={password}
+              placeholder=""
+              onChange={handlePasswordChange}
+            />
+          </div>
+          <div className="mb-3">
+            <div className="custom-control custom-checkbox">
+              <input
+                type="checkbox"
+                className="custom-control-input"
+                id="customCheck1"
+              />
+              <label className="custom-control-label" htmlFor="customCheck1">
+                Remember me
+              </label>
+            </div>
+          </div>
+          <div className="d-grid">
+            <button type="submit" className="btn btn-primary">
+              Glory to Ohio
+            </button>  
+            <h3>{token}</h3>       
+          </div>
+          <p className="forgot-password text-right">
+            Forgot <a href="https://www.youtube.com/watch?v=Zt-Ql9ZE2XI">password?</a>
+          </p>
+        </form>
         </div>
-        <div className="d-grid">
-          <button type="submit" className="btn btn-primary"onClick={handleClick}>
-            Glory to Ohio
-          </button>
-          <h3 className='joke'>{data.value}</h3>
         </div>
-        <p className="forgot-password text-right">
-          Forgot <a href="https://www.youtube.com/watch?v=Zt-Ql9ZE2XI">password?</a>
-        </p>
-      </form>
+  
       </div>
+    );
+  }
+  else{
+    return(
+      <div>
+        zalogowano
       </div>
-    </div>
-  );
+    )
+  }
+
+
 }
 function Login(){
 
