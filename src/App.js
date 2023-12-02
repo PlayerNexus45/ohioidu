1d49275b772f59162660aba2b285236bc4ea8fe8
PlayerNexus45 <80288457+PlayerNexus45@users.noreply.github.com>
2023-12-03 00:09:35 +0100

    clean

--- src/App.js
+++ src/App.js
@@ -4,33 +4,33 @@ import axios from 'axios'
 import React from 'react';
 import Login from './components/login.components';
 import useToken from './components/useToken';
+
 function Profile(token){
   axios.get('https://s35.idu.edu.pl/api/v2/user/profile', {headers: {'X-API-TOKEN': token}})
   .then((response) =>{
     sessionStorage.setItem('profile', response.data.data)
+
     const profile = response.data.data
-    var str = JSON.stringify(response.data.data)
     const element = document.getElementById('profile')
-    element.innerHTML = `${str} <br/> Citizen ${profile.first_name} ${profile.last_name} ID: ${profile.id}, <br/> You have ${profile.unread_messages_count} unread messages, see to them at Your earliest convenience, <br/> `
-  }
-  )
+
+    element.innerHTML = `Citizen ${profile.first_name} ${profile.last_name} ID: ${profile.id}, <br/> You have ${profile.unread_messages_count} unread messages, see to them at Your earliest convenience`
+  })
 }
 
 function App() {
   const {token, setToken} = useToken();
+
   if(!token){
     return <Login setToken={setToken}/>
   }
+
   Profile(token)
+  
   return(
     <div className='IDU'>
       <h1>Profile</h1>
       <h2 id='profile'>Loading...</h2>
     </div>
  )
-  
-
-
 }
-
 export default App;
